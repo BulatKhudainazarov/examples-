ec6e38d2d0b8304ab52e62871b8f89764dfa5301
Bulat <hudainazarov.b@yandex.ru>
2020-01-11 18:25:27 +0200

    slick-slider

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,9 +1,37 @@
-<script>
-
-  $(document).ready(function () {
-    $('.header-contacts__button').on('click', function() {
+$(document).ready(function () {
+  $('.header-contacts__button').on('click', function() {
       $('.overlay').show()
-    })
-  })
+    });
 
-</script>
+    $('.popup-close').on('click', function() {
+      $('.overlay').hide()
+  });
+  $('.feedbacks-block').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-nav',
+    dots: false,
+    speed: 1000,
+    infinite: true,
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider',
+    dots: false,
+    focusOnSelect: true,
+    arrows: false,
+    infinite: false,
+  });
+  $('.main-img').slick({
+    asNavFor: '.slick-blocks',
+  })
+  $('.slick-blocks').slick({
+    arrows: false,
+    asNavFor: '.main-img',
+  })
+})
